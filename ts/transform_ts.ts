ce36cfac857dd75e0da2aae9a5df22523655ae13
Andrew Peters <andrew.peters.dev@gmail.com>
2023-06-20 11:14:12 +0200

    add task for bundle

--- ts/transform_ts.ts
+++ ts/transform_ts.ts
@@ -4,19 +4,20 @@
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
 import * as esbuild from "https://deno.land/x/esbuild@v0.17.19/mod.js";
 
-const [entrypoint = "", js_out = ""] = Deno.args;
+const [entrypoint = "ts/mathjax/index.ts", js_out = "./dist/mathjax.js"] =
+  Deno.args;
 
 // support Qt 5.14
 const target = ["es6", "chrome77"];
 
 const result = await esbuild.build({
-    plugins: [...denoPlugins()],
-    bundle: false,
-    entryPoints: [entrypoint],
-    outfile: js_out,
-    minify: true,
-    preserveSymlinks: true,
-    target,
+  plugins: [...denoPlugins()],
+  bundle: false,
+  entryPoints: [entrypoint],
+  outfile: js_out,
+  minify: true,
+  preserveSymlinks: true,
+  target,
 });
 
 console.log(result.outputFiles);
